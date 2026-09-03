173a5e3e9acb70ecf0fc83905edc9f964ef067e8
liuhe1996a-tech <liuhe1996a@gmail.com>
2026-09-03 13:10:33 +0800

    Delete AI_upgrade directory

--- environment/install_R_packages.R
+++
@@ -1,3 +0,0 @@
-if (!requireNamespace("BiocManager", quietly=TRUE)) install.packages("BiocManager")
-BiocManager::install(version="3.22", c("DESeq2","edgeR","fgsea"), ask=FALSE)
-install.packages(c("data.table","ggplot2","cowplot","readr","dplyr"))
